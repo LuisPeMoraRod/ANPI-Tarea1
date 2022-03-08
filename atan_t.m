d1cb4ead1de553c7c4581e2ef0c60259ce0bc0a4
Luis Pedro Morales <luismorarod98@gmail.com>
2022-03-07 20:44:30 -0600

    Add atan_t

--- atan_t.m
+++ atan_t.m
@@ -1,21 +1,21 @@
-#Function that computes sin(x) 
+#Function that computes arctan(x) 
 #parameters: 
   # @x : preimage
-#output: result of the operation sin(x)
-function [result] = sin_t(x)
+#output: result of the operation arctan(x)
+function [result] = atan_t(x)
   tol = 1e-8; # generic tolerance defined for the package
   iterMax = 2500; # generic maximum iterations defined for the package
   a = x;
   
   function [s_k] = get_S_k(n)
-    s_k = (-1)^n * a^(2*n+1) * div_t(factorial(2*n+1)); # defined succession for aproximation
+    s_k = (-1)^n * a^(2*n+1) * div_t(2*n+1); # defined succession for aproximation
   endfunction
   
   s_k = get_S_k(0); #initial S_0
   for n = 1 : iterMax -1
       s_kp1 = s_k + get_S_k(n); #sum of last value and new value of the iteration
       error = abs(s_kp1 - s_k); # error calculation
-      s_k = s_kp1;
+      s_k = s_kp1
       if (error < tol ) # stopping condition
         break;
       endif
--- sinh.m
+++ sinh.m
@@ -1,17 +1,17 @@
-#Function that computes sin(x) 
+#Function that computes sinh(x) 
 #parameters: 
   # @x : preimage
-#output: result of the operation sin(x)
-function [result] = sin_t(x)
+#output: result of the operation sinh(x)
+function [result] = sinh(x)
   tol = 1e-8; # generic tolerance defined for the package
   iterMax = 2500; # generic maximum iterations defined for the package
   a = x;
   
   function [s_k] = get_S_k(n)
-    s_k = (-1)^n * a^(2*n+1) * div_t(factorial(2*n+1)); # defined succession for aproximation
+    s_k = a^(2*n+1) * div_t(factorial(2*n+1)); # defined succession for aproximation
   endfunction
   
-  s_k = get_S_k(0); #initial S_0
+  s_k = get_S_k(0); #initial s_0
   for n = 1 : iterMax -1
       s_kp1 = s_k + get_S_k(n); #sum of last value and new value of the iteration
       error = abs(s_kp1 - s_k); # error calculation
@@ -22,3 +22,4 @@ function [result] = sin_t(x)
     endfor
   result = s_k;
   endfunction
+  
