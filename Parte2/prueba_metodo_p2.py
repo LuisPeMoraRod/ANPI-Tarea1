10f16453ccb8c8e29a4ea213b893cbff724ff687
irefut <irefut@gmail.com>
2022-03-19 11:38:26 -0600

    Listo con los prints acordados

--- Parte2/prueba_metodo_p2.py
+++ Parte2/prueba_metodo_p2.py
@@ -1,20 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Prueba de métodos implementados
+"""
 from metodos_p2 import *
+
+
+def p_newton_H_m1():
+    f_prueba = 'cos(x)-x'
+    n_metodo = "Kanwar-Tomar"
+    sol = newton_H_m1(f_prueba,1,10**-5,500)
+    print("Función a calcular el cero: f(x) =",f_prueba)
+    print("Utilizando Método",n_metodo)
+    print("Aproximación de la solución: xk =", sol[0])
+    print("Con un error de ", sol[1])
+    print('\n')
+    
+def p_newton_H_m2():
+    f_prueba = 'cos(x)-x'
+    n_metodo = "Newton"
+    sol = newton_H_m2(f_prueba,1,10**-5,500)
+    print("Función a calcular el cero: f(x) = ",f_prueba)
+    print("Utilizando Método",n_metodo)
+    print("Aproximación de la solución: xk = ", sol[0])
+    print("Con un error de", sol[1])
+    print('\n')
 
 def p_newton_G_m1():
     f_prueba = 'cos(x)-x'
     n_metodo = "Halley"
-    print("La función a la que se le calculara el cero es:",f_prueba)
-    print("El método usado es: ",n_metodo)
-    print("La aproximacion de la solucion es: xk = ",newton_G_m1(f_prueba,1,10**-5,500)[0])
-    print("El error es: ",newton_G_m1(f_prueba,1,10**-5,500)[1])
+    print("Función a calcular el cero: f(x) =",f_prueba)
+    print("Utilizando Método",n_metodo)
+    print("Aproximacion de la solucion: xk =",newton_G_m1(f_prueba,1,10**-5,500)[0])
+    print("Con un error de",newton_G_m1(f_prueba,1,10**-5,500)[1])
+    print('\n')
 
 def p_newton_G_m2():
     f_prueba = 'cos(x)-x'
     n_metodo = "Chebyshev"
-    print("La función a la que se le calculara el cero es:",f_prueba)
-    print("El método usado es: ",n_metodo)
-    print("La aproximacion de la solucion es: xk = ",newton_G_m2(f_prueba,1,10**-5,500)[0])
-    print("El error es: ",newton_G_m2(f_prueba,1,10**-5,500)[1])
+    print("Función a calcular el cero: f(x) =",f_prueba)
+    print("Utilizando Método",n_metodo)
+    print("Aproximacion de la solucion: xk = ",newton_G_m2(f_prueba,1,10**-5,500)[0])
+    print("Con un error de",newton_G_m2(f_prueba,1,10**-5,500)[1])
 
+p_newton_H_m1()
+p_newton_H_m2()
 p_newton_G_m1()
 p_newton_G_m2()
